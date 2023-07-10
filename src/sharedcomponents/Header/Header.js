2f41d5b106c31e524cc0b07dea868a8b5c58b22a
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-07-11 00:26:08 +0600

    header added

--- src/sharedcomponents/Header/Header.js
+++ src/sharedcomponents/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
     return (
@@ -8,45 +9,16 @@ export const Header = () => {
                     <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
                         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
                             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto">
-                                <span className="relative inline-block">
-                                    <svg
-                                        viewBox="0 0 52 24"
-                                        fill="currentColor"
-                                        className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-deep-purple-accent-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-                                    >
-                                        <defs>
-                                            <pattern
-                                                id="700c93bf-0068-4e32-aafe-ef5b6a647708"
-                                                x="0"
-                                                y="0"
-                                                width=".135"
-                                                height=".30"
-                                            >
-                                                <circle cx="1" cy="1" r=".7" />
-                                            </pattern>
-                                        </defs>
-                                        <rect
-                                            fill="url(#700c93bf-0068-4e32-aafe-ef5b6a647708)"
-                                            width="52"
-                                            height="24"
-                                        />
-                                    </svg>
-                                    <span className="relative">The</span>
-                                </span>{' '}
-                                quick, brown fox jumps over a lazy dog
+                                Ride the Wave of Knowledge with <span className='underline'>CourseWave</span>
                             </h2>
                             <p className="text-base text-indigo-100 md:text-lg">
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                                accusantium doloremque rem aperiam, eaque ipsa quae.
+                                Dive into a Vast Ocean of Courses: Discover Your Perfect Wave of Education with CourseWave, the Ultimate Destination for Lifelong Learning!
                             </p>
                         </div>
                         <div>
-                            <a
-                                href="/"
-                                className="inline-flex items-center justify-center h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-deep-purple-accent-100 focus:shadow-outline focus:outline-none"
-                            >
-                                Get started
-                            </a>
+                            <Link to='/courses' className="inline-flex items-center justify-center h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-deep-purple-accent-100 focus:shadow-outline focus:outline-none">
+                                View Courses
+                            </Link>
                         </div>
                     </div>
                 </div>
