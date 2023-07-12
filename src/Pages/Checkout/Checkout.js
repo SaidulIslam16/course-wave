3a2c577a948c0fbe2aa53b3cfdeaf1502fd5aa5c
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-07-12 10:44:32 +0600

    preparing for firebase deploy

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -24,7 +24,7 @@ const Checkout = () => {
                     <span className=" block text-sm font-medium text-slate-700">
                         Email
                     </span>
-                    <input type="email" name="email" value={email} disabled className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter name" />
+                    <input type="email" name="email" value={email} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter name" />
                 </label>
                 <label className="block ">
                     <span className=" block text-sm font-medium text-slate-700">
